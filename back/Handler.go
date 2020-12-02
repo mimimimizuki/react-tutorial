cac33acad1b021272c08ca0d0983bb7bb713e4e5
mimimimizuki <mizuki0327.5@gmail.com>
2020-12-03 03:00:39 +0900

    handle go lint warning and create API

--- back/Handler.go
+++ back/Handler.go
@@ -13,6 +13,8 @@ import (
 )
 
 // ============> Post
+
+// GetPosts is return all posts
 func GetPosts(w http.ResponseWriter, r *http.Request) {
 	log.Printf("Get all posts")
 	var post Post
@@ -35,20 +37,32 @@ func GetPosts(w http.ResponseWriter, r *http.Request) {
 	}
 	json.NewEncoder(w).Encode(Posts)
 }
+
+// GetPost is return one user's posts
 func GetPost(w http.ResponseWriter, r *http.Request) {
 	log.Println("Get post is called")
 	var post Post
 	params := mux.Vars(r)
-	log.Println(params["id"])
-	rows := A.DB.QueryRow("select * from posts where user_id = $1", params["id"])
-	// rows := A.DB.QueryRow("select * from posts where user_id = 1")
-	err := rows.Scan(&post.ID, &post.UserId, &post.PostDate, &post.Title, &post.Overview, &post.Link, &post.Thought, pq.Array(&post.Tags))
+	Posts = []Post{}
+	rows, err := A.DB.Query("select * from posts where user_id = $1", params["id"])
 	if err != nil {
+		log.Fatal(err)
+	}
+	defer rows.Close()
+	for rows.Next() {
+		err := rows.Scan(&post.ID, &post.UserId, &post.PostDate, &post.Title, &post.Overview, &post.Link, &post.Thought, pq.Array(&post.Tags))
+		if err != nil {
+			log.Fatal(err)
+		}
+		Posts = append(Posts, post)
+	}
+	if err = rows.Err(); err != nil {
 		log.Println(err)
 	}
-	log.Println(&post.PostDate)
-	json.NewEncoder(w).Encode(&post)
+	json.NewEncoder(w).Encode(Posts)
 }
+
+// AddPost is to add posts of one users
 func AddPost(w http.ResponseWriter, r *http.Request) {
 	post := Post{}
 	var postID int
@@ -59,15 +73,15 @@ func AddPost(w http.ResponseWriter, r *http.Request) {
 	post.Overview = r.FormValue("Overview")
 	post.Link = r.FormValue("Link")
 	post.Thought = r.FormValue("Thought")
-	log.Println(r.FormValue("Tags"))
-	// post.Tags = (r.FormValue("Tags"))
 	err := A.DB.QueryRow("INSERT INTO posts (user_id, post_time , title, overview, link, thought, tags) values($1, $2, $3, $4, $5, $6 , $7) RETURNING post_id;",
-		post.UserId, post.PostDate, post.Title, post.Overview, post.Link, post.Thought, pq.Array(post.Tags)).Scan(&postID)
+		post.UserId, post.PostDate, post.Title, post.Overview, post.Link, post.Thought, pq.Array(r.FormValue("Tags"))).Scan(&postID)
 	if err != nil {
 		log.Fatal(err)
 	}
 	json.NewEncoder(w).Encode(postID)
 }
+
+// UpdatePost is to update a post which one users posted
 func UpdatePost(w http.ResponseWriter, r *http.Request) {
 	var post Post
 	log.Println("update post is called")
@@ -85,12 +99,14 @@ func UpdatePost(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(rowsUpdated)
 
 }
+
+// RemovePost is to delete a post which one user want to delete
 func RemovePost(w http.ResponseWriter, r *http.Request) {
 	log.Println("Remove post is called")
 	params := mux.Vars(r)
 	result, err := A.DB.Exec("DELETE FROM posts WHERE post_id = $1", params["id"])
 	if err != nil {
-		log.Println("err")
+		log.Println(err)
 	}
 	rowsDeleted, err := result.RowsAffected()
 	if err != nil {
@@ -102,6 +118,7 @@ func RemovePost(w http.ResponseWriter, r *http.Request) {
 
 // ============> User
 
+// AddUser is to create new account
 func AddUser(w http.ResponseWriter, r *http.Request) {
 	var user User
 	var userID int
@@ -111,14 +128,15 @@ func AddUser(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		log.Fatal(err)
 	}
-	err_1 := A.DB.QueryRow("INSERT INTO users (display_name, birthday, pass, bio) values ($1 , $2 , $3, $4) RETURNING user_id;",
+	err := A.DB.QueryRow("INSERT INTO users (display_name, birthday, pass, bio) values ($1 , $2 , $3, $4) RETURNING user_id;",
 		user.DisplayName, user.Birthday, string(hash), user.BIO).Scan(&userID)
-	if err_1 != nil {
+	if err != nil {
 		log.Fatal(err)
 	}
 	json.NewEncoder(w).Encode(userID)
 }
 
+// GetUsers is maybe not used
 func GetUsers(w http.ResponseWriter, r *http.Request) {
 	log.Printf("Get all users")
 	var user User
@@ -140,13 +158,14 @@ func GetUsers(w http.ResponseWriter, r *http.Request) {
 	}
 	json.NewEncoder(w).Encode(Users)
 }
+
+// GetUser is to get one user infomation about diaplayname or bio ...
 func GetUser(w http.ResponseWriter, r *http.Request) {
 	log.Println("Get user")
 	var user User
 	params := mux.Vars(r)
 	log.Println(params["id"])
 	rows := A.DB.QueryRow("select * from users where user_id = $1", params["id"])
-	// rows := A.DB.QueryRow("select * from posts where user_id = 1")
 	err := rows.Scan(&user.ID, &user.DisplayName, &user.Birthday, &user.Pass, &user.BIO)
 	if err != nil {
 		log.Println(err)
@@ -154,6 +173,8 @@ func GetUser(w http.ResponseWriter, r *http.Request) {
 
 	json.NewEncoder(w).Encode(&user)
 }
+
+// UpdateUser is to update user infomation
 func UpdateUser(w http.ResponseWriter, r *http.Request) { //dont use this API yet
 	var user User
 	log.Println("update user is called")
@@ -168,6 +189,8 @@ func UpdateUser(w http.ResponseWriter, r *http.Request) { //dont use this API ye
 	}
 	json.NewEncoder(w).Encode(rowsUpdated)
 }
+
+// RemoveUser is to delete account
 func RemoveUser(w http.ResponseWriter, r *http.Request) {
 	log.Println("delete user is called")
 	params := mux.Vars(r)
@@ -182,9 +205,30 @@ func RemoveUser(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("rowsDeleted", rowsDeleted)
 	json.NewEncoder(w).Encode(rowsDeleted)
 }
-func GetWantReads(w http.ResponseWriter, r *http.Request) {
 
+// ============> wantread
+
+// GetWantReads is maybe not used
+func GetWantReads(w http.ResponseWriter, r *http.Request) {
+	var wantread WantRead
+	WantReads = []WantRead{}
+	log.Println("get wantread is called")
+	rows, err := A.DB.Query("select * from wantreads")
+	defer rows.Close()
+	for rows.Next() {
+		err := rows.Scan(&wantread.ID, &wantread.UserId, &wantread.Title, &wantread.Link)
+		if err != nil {
+			log.Fatal(err)
+		}
+		WantReads = append(WantReads, wantread)
+	}
+	if err = rows.Err(); err != nil {
+		log.Fatal(err)
+	}
+	json.NewEncoder(w).Encode(WantReads)
 }
+
+// GetWantRead is to return papers which one user to want to read
 func GetWantRead(w http.ResponseWriter, r *http.Request) {
 	var wantread WantRead
 	WantReads = []WantRead{}
@@ -204,14 +248,54 @@ func GetWantRead(w http.ResponseWriter, r *http.Request) {
 	}
 	json.NewEncoder(w).Encode(WantReads)
 }
+
+// AddWantRead is to add user's wanna read paper
 func AddWantRead(w http.ResponseWriter, r *http.Request) {
-
+	wantread := WantRead{}
+	var wantreadID int
+	log.Println("add want read is called")
+	wantread.UserId, _ = strconv.Atoi(r.FormValue("UserId"))
+	wantread.Title = r.FormValue("Title")
+	wantread.Link = r.FormValue("Link")
+	err := A.DB.QueryRow("INSERT INTO wantreads (user_id, title, link) values ($1, $2, $3) RETURNING wantread_id;",
+		wantread.UserId, wantread.Title, wantread.Link).Scan(&wantreadID)
+	if err != nil {
+		log.Fatal(err)
+	}
+	json.NewEncoder(w).Encode(wantreadID)
 }
+
+// UpdateWantRead is maybe used for change this paper to post(already read)
 func UpdateWantRead(w http.ResponseWriter, r *http.Request) {
-
+	var wantread WantRead
+	log.Println("update wantread is called")
+	json.NewDecoder(r.Body).Decode(&wantread)
+	result, err := A.DB.Exec("UPDATE wantreads SET title=$1, link=$2 WHERE wantread_id = $3",
+		&wantread.Title, &wantread.Link, &wantread.ID)
+	if err != nil {
+		log.Fatal(err)
+	}
+	rowsUpdated, err := result.RowsAffected()
+	if err != nil {
+		log.Fatal(err)
+	}
+	json.NewEncoder(w).Encode(rowsUpdated)
 }
-func RemoveWantRead(w http.ResponseWriter, r *http.Request) {
 
+// RemoveWantRead is maybe used for change this paper to post(already read)
+func RemoveWantRead(w http.ResponseWriter, r *http.Request) {
+	log.Println("delete wantread is called")
+	params := mux.Vars(r)
+	result, err := A.DB.Exec("DELETE FROM wantreads WHERE wantread_id = $1", params["id"])
+	if err != nil {
+		log.Fatal(err)
+	}
+	rowsDeleted, err := result.RowsAffected()
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("rowsDeleted", rowsDeleted)
+	json.NewEncoder(w).Encode(rowsDeleted)
 }
 
 // func passwordVerify(hash, pw string) error {
